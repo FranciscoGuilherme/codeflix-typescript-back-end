a9a2e470160df73ceb48f3b0516e4b01c3b9b94a
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 13:41:36 -0300

    test(domain): :white_check_mark: normaliza o teste de unidade referente a entidade categoria.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,8 @@
 import { Category } from "./category";
 
 describe("Unit tests for entity Category", (): void => {
-    it("should instantiate Category", (): void => {
-        const category: Category = new Category("Movie");
-        expect(category.name).toEqual("Movie");
-    });
+  it("should instantiate Category", (): void => {
+    const category: Category = new Category({ name: "Movie" });
+    expect(category.props.name).toEqual("Movie");
+  });
 });
