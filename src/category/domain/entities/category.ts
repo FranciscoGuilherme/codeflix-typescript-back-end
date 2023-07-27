31123026ad6db56abad13e75054865e133964eaa
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 13:41:13 -0300

    feat(domain): :sparkles: implementa novos atributos para a entidade categoria.

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,12 @@
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  isActive?: boolean;
+  createdAt?: Date;
+};
+
 export class Category {
-    constructor(public name: string) {}
+  constructor(public readonly props: CategoryProperties) {
+    Object.assign(this, props);
+  }
 }
