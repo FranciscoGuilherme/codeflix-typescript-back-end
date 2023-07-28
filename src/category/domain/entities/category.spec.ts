77958798f66b90003e3a39f1ea8af715fbb95698
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-28 18:02:39 -0300

    test(domain): :white_check_mark: implementa novos testes de unidade referente a entidade categoria.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -3,6 +3,33 @@ import { Category } from "./category";
 describe("Unit tests for entity Category", (): void => {
   it("should instantiate Category", (): void => {
     const category: Category = new Category({ name: "Movie" });
-    expect(category.props.name).toEqual("Movie");
+
+    expect(category).toBeDefined();
+    expect(category).toBeInstanceOf(Category);
+  });
+
+  it("should validate constructor category with default values", (): void => {
+    const category: Category = new Category({ name: "Movie" });
+
+    expect(category.props.name).toBe("Movie");
+    expect(category.props.description).toBeUndefined();
+    expect(category.props.isActive).toBeUndefined();
+    expect(category.props.createdAt).toBeUndefined();
+  });
+
+  it("should validate constructor category", (): void => {
+    const date = new Date();
+    const category: Category = new Category({
+      name: "Movie",
+      description: "Some description",
+      isActive: false,
+      createdAt: date
+    });
+
+    expect(category.props.name).toBe("Movie");
+    expect(category.props.description).toBe("Some description");
+    expect(category.props.isActive).toBeFalsy();
+    expect(category.props.createdAt).toBe(date);
+    expect(category.props.createdAt).toBeInstanceOf(Date);
   });
 });
