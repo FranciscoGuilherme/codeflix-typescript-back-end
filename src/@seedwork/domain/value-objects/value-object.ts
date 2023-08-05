8ee9136e5577c7f64a2487ad0be252e412613ed0
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-05 16:34:44 -0300

    feat(value-objects): :sparkles: torna os objetos de valor do sistema imutaveis.

--- src/@seedwork/domain/value-objects/value-object.ts
+++ src/@seedwork/domain/value-objects/value-object.ts
@@ -1,27 +1,21 @@
+import { deepFreeze } from "@seedwork/domain/utils/object";
+
 export default abstract class ValueObject<Value = any> {
-  protected _value: Value;
+  protected readonly _value: Value;
 
   protected constructor(value: Value) {
-    this._value = value;
+    this._value = deepFreeze(value);
   }
 
   get value(): Value {
     return this._value;
   }
 
-  toString = (): string => {
-    if (this.value === null) {
-      return "null";
-    }
-
+  toString: () => string = (): string => {
     if (typeof this.value === "object" && !(this.value instanceof Date)) {
       return JSON.stringify(this.value);
     }
 
-    try {
-      return this.value.toString();
-    } catch (e) {
-      return `${this.value}`;
-    }
+    return this.value.toString();
   };
 }
