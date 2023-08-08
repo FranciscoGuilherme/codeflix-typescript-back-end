ac25bdbb6c48db9b04cbe29783144d25db93562b
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-08 18:04:35 -0300

    feat(seedwork): :sparkles: implementa as validacoes com o 'class-validator'.

---
+++ src/@seedwork/validators/class-validator-fields.ts
@@ -0,0 +1,26 @@
+import { validateSync, ValidationError } from "class-validator";
+import ValidatorFieldsInterface, { FieldsErrors } from "@seedwork/validators/validator-fields-interface";
+
+class ClassValidatorFields<PropsValidated> implements ValidatorFieldsInterface<PropsValidated> {
+  errors: FieldsErrors;
+  validatedData: PropsValidated;
+
+  validate(data: any): boolean {
+    const errors: ValidationError[] = validateSync(data);
+
+    if(errors.length) {
+      this.errors = {};
+
+      errors.forEach((error: ValidationError): void => {
+        const field: string = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      });
+    }
+
+    if(!errors.length) {
+      this.validatedData = data;
+    }
+
+    return !errors.length;
+  }
+}
