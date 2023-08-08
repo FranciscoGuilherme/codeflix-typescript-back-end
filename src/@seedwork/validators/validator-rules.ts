ba16b6a9fa851582b9917c12d7c595417e0ec178
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-08 11:31:27 -0300

    feat(seedwork): :sparkles: implementa as validacoes que servirao as entidades do core.

---
+++ src/@seedwork/validators/validator-rules.ts
@@ -0,0 +1,35 @@
+import ValidationError from "@seedwork/errors/validation-error";
+
+export default class ValidatorRules {
+  private constructor(private value: any, private property: string) {}
+
+  static values(value: any, property: string): ValidatorRules {
+    return new ValidatorRules(value, property);
+  }
+
+  required(): this {
+    if(this.value === null || this.value === undefined || this.value === "") {
+      throw new ValidationError(`The ${this.property} is required`);
+    }
+
+    return this;
+  }
+
+  string(): this {
+    if(typeof this.value !== "string") {
+      throw new ValidationError(`The ${this.property} must be a string`);
+    }
+
+    return this;
+  }
+
+  maxLength(max: number): this {
+    if(this.value.length > max) {
+      throw new ValidationError(
+        `The ${this.property} must be less or equal then ${max} characters`
+      );
+    }
+
+    return this;
+  }
+}
