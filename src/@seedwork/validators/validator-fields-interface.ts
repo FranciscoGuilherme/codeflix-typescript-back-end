45cd74af92126c30994e0d5be836e3010dbd576c
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-08 18:04:01 -0300

    feat(seedwork): :sparkles: implementa o limite arquitetural parcial das validacoes com a interface.

---
+++ src/@seedwork/validators/validator-fields-interface.ts
@@ -0,0 +1,10 @@
+export type FieldsErrors = {
+  [field: string]: string[];
+};
+
+export default interface ValidatorFieldsInterface<PropsValidated> {
+  errors: FieldsErrors;
+  validatedData: PropsValidated;
+
+  validate(data: any): boolean;
+}
