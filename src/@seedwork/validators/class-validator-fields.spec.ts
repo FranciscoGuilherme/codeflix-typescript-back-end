883e1f20e04e88928d75fd96e328e9e2080904e6
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-09 11:03:11 -0300

    test(validators): :white_check_mark: implementa os testes de unidade referentes a classe generica do 'class-validator'.

---
+++ src/@seedwork/validators/class-validator-fields.spec.ts
@@ -0,0 +1,40 @@
+import * as libClassValidator from "class-validator";
+import { ClassValidatorFields } from "@seedwork/validators/class-validator-fields";
+
+class StubClassValidator extends ClassValidatorFields<{ field: string }> {}
+
+describe("Unit tests for class validator generic class", (): void => {
+  it("should initialize errors and validatedData variables with null", (): void => {
+    const validator: StubClassValidator = new StubClassValidator();
+
+    expect(validator.errors).toBeNull();
+    expect(validator.validatedData).toBeNull();
+  });
+
+  it("should validate with errors", (): void => {
+    const validator: StubClassValidator = new StubClassValidator();
+    const spyValidateSync: jest.SpyInstance<libClassValidator.ValidationError[]> =
+      jest.spyOn(libClassValidator, "validateSync").mockReturnValue([
+        {
+          property: "field",
+          constraints: { isRequired: "some error" }
+        }
+      ]);
+
+    expect(validator.validatedData).toBeNull();
+    expect(validator.validate(null)).toBeFalsy();
+    expect(validator.errors).toStrictEqual({ field: ["some error"] });
+    expect(spyValidateSync).toHaveBeenCalled();
+  });
+
+  it("should validate without errors", (): void => {
+    const validator: StubClassValidator = new StubClassValidator();
+    const spyValidateSync: jest.SpyInstance<libClassValidator.ValidationError[]> =
+      jest.spyOn(libClassValidator, "validateSync").mockReturnValue([]);
+
+    expect(validator.errors).toBeNull();
+    expect(validator.validate({ field: "value" })).toBeTruthy();
+    expect(validator.validatedData).toStrictEqual({ field: "value" });
+    expect(spyValidateSync).toHaveBeenCalled();
+  });
+});
