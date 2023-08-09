9014c8ee9c7d75905321c446ef0648f0e46abce6
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-09 16:07:37 -0300

    feat(seedwork): :sparkles: implementa novo tipo de excecao.

--- src/@seedwork/domain/errors/validation-error.ts
+++ src/@seedwork/domain/errors/validation-error.ts
@@ -1,1 +1,10 @@
-export default class ValidationError extends Error {}
+import { FieldsErrors } from "@seedwork/domain/validators/validator-fields-interface";
+
+export class ValidationError extends Error {}
+
+export class EntityValidationError extends Error {
+  constructor(public readonly error: FieldsErrors) {
+    super("Entity Validation Error");
+    this.name = "EntityValidationError";
+  }
+}
