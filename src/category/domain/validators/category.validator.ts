b570bbe6ef7625daf73139e68232ec46eb68680a
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-09 10:36:06 -0300

    feat(validators): :sparkles: implementa as validacoes para a entidade categoria.

---
+++ src/category/domain/validators/category.validator.ts
@@ -0,0 +1,39 @@
+import { CategoryProperties } from "../entities/category";
+import { ClassValidatorFields } from "@seedwork/validators/class-validator-fields";
+import {
+  IsDate,
+  IsString,
+  MaxLength,
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional
+} from "class-validator";
+
+export class CategoryRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
+
+  constructor(data: CategoryProperties) {
+    Object.assign(this, data);
+  }
+}
+
+export class CategoryValidator extends ClassValidatorFields<CategoryRules> {
+  validate(data: CategoryRules): boolean {
+    return super.validate(new CategoryRules(data));
+  }
+}
