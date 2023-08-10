3c392eef33101bbe03751fe65c7933eec02da23a
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-10 10:24:21 -0300

    feat(tests): :sparkles: implementa passagem de funcao para a rotina custom de teste.

--- src/@seedwork/domain/tests/validations.ts
+++ src/@seedwork/domain/tests/validations.ts
@@ -1,38 +1,58 @@
 import CustomMatcherResult = jest.CustomMatcherResult;
 import { FieldsErrors } from "@seedwork/domain/validators/validator-fields-interface";
 import { ClassValidatorFields } from "@seedwork/domain/validators/class-validator-fields";
+import { EntityValidationError } from "@seedwork/domain/errors/validation-error";
 
-type Expected = { validator: ClassValidatorFields<any>; data: any; };
+type Expected = { validator: ClassValidatorFields<any>; data: any; } | (() => any);
 
 expect.extend({
   containsErrorMessage(
     expected: Expected,
     received: FieldsErrors
   ): CustomMatcherResult | Promise<CustomMatcherResult> {
+    const successScenario: CustomMatcherResult = {
+      pass: false,
+      message: (): string => "The data is valid"
+    };
+
+    if(typeof expected === "function") {
+      try {
+        expected();
+
+        return successScenario;
+      } catch(error: any) {
+        const e: EntityValidationError = error as EntityValidationError;
+
+        return checkIsMatch(received, e.error);
+      }
+    }
+
     const isValid: boolean = expected.validator.validate(expected.data);
 
     if(isValid) {
-      return {
-        pass: false,
-        message: (): string => "The data is valid"
-      };
+      return successScenario;
     }
 
-    const isMatch = expect
-      .objectContaining(received)
-      .asymmetricMatch(expected.validator.errors);
-
-    if(!isMatch) {
-      return {
-        pass: true,
-        message: (): string => {
-          const errors: string = JSON.stringify(expected.validator.errors);
-
-          return `The validations errors not contains ${JSON.stringify(errors)}`
-        }
-      };
-    }
-
-    return { pass: true,  message: (): string => "" };
+    return checkIsMatch(received, expected.validator.errors);
   }
 });
+
+const checkIsMatch = (
+  errors: FieldsErrors,
+  received: FieldsErrors
+): CustomMatcherResult | Promise<CustomMatcherResult> => {
+  const isMatch = expect.objectContaining(received).asymmetricMatch(errors);
+
+  if(!isMatch) {
+    return {
+      pass: true,
+      message: (): string => {
+        const e: string = JSON.stringify(errors);
+
+        return `The validations errors not contains ${JSON.stringify(e)}`
+      }
+    };
+  }
+
+  return { pass: true,  message: (): string => "" };
+};
